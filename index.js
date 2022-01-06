dd039714dc58e2ad191c4122eb5da2a4480ae774
kalyancharlie <kalyancharlie@gmail.com>
2022-01-06 16:50:47 +0530

    heroku dyanmic port r10 fix

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const app = require("./src/app");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+    console.log(`Server started at ${port}`);
 });
